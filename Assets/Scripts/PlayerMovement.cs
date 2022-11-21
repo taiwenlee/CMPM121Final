a875f89bdb2a117fd5209066f55b1378f7410aff
Soiiz <kchau6@ucsc.edu>
2022-11-20 20:08:30 -0800

    More player stuff added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,30 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private PlayerInput playerInput;
     public float speed = 5f;
     public float gravity = -1.0f;
-
+    private float movementX;
+    private float movementY;
     Vector3 velocity;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+    }
+    private void OnMove(InputValue value)
+    {
+       Vector2 move = value.Get<Vector2>();
+
+       movementX = move.x;
+       movementY = move.y;
     }
     private void Update()
     {
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
+        Vector3 move = new Vector3(movementX, 0, movementY);
         characterController.Move(move * Time.deltaTime * speed);
     }
 }
